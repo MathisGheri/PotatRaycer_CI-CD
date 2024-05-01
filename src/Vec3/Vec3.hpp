699d92060b7f95a78b2e61f28cdaaaac9f170d3e
Suika <camille.ricardon@epitech.eu>
2024-05-01 15:52:19 +0200

    [~] move simple declaration from Vec3.cpp to Vec3.hpp

--- src/Vec3/Vec3.hpp
+++ src/Vec3/Vec3.hpp
@@ -47,4 +47,115 @@ class Vec3 {
     private:
 };
 
+/*******************Out of Scope*******************/
+inline std::istream& operator>>(std::istream &is, Vec3 &t)
+{
+ is >> t.e[0] >> t.e[1] >> t.e[2];
+ return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, Vec3 &t)
+{
+ os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+ return os;
+}
+
+inline void Vec3::make_unit_vector()
+{
+	float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+	e[0] *= k; e[1] *= k; e[2] *= k;
+}
+
+inline Vec3 operator+(const Vec3 &v1, const Vec3 &v2)
+{
+ 	return Vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline Vec3 operator-(const Vec3 &v1, const Vec3 &v2)
+{
+ 	return Vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+inline Vec3 operator*(const Vec3 &v1, const Vec3 &v2)
+{
+ 	return Vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+inline Vec3 operator/(const Vec3 &v1, const Vec3 &v2)
+{
+ 	return Vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline Vec3 operator*(float t, const Vec3 &v)
+{
+ 	return Vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline Vec3 operator*(const Vec3 &v,float t)
+{
+ 	return Vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline Vec3 operator/(float t, const Vec3 &v)
+{
+ 	return Vec3(t / v.e[0], t / v.e[1], t / v.e[2]);
+}
+
+inline Vec3 operator/(const Vec3 &v, float t)
+{
+ 	return Vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
+}
+
+inline Vec3 cross(const Vec3 &v1, const Vec3 &v2)
+{
+	return Vec3(v1.e[1] * v2.e[2] - v1.e[2] * v2[1], 
+	-(v1.e[0] * v2.e[2] - v1.e[2] * v2[0]), 
+	v1.e[0] * v2.e[1] - v1.e[1] * v2[0]);
+}
+
+inline Vec3& Vec3::operator+=(const Vec3 &v)
+{
+	e[0] += v.e[0];
+	e[1] += v.e[1];
+	e[2] += v.e[2];
+	return *this;
+}
+
+inline Vec3& Vec3::operator-=(const Vec3 &v)
+{
+	e[0] -= v.e[0];
+	e[1] -= v.e[1];
+	e[2] -= v.e[2];
+	return *this;
+}
+
+inline Vec3& Vec3::operator*=(const Vec3 &v)
+{
+	e[0] *= v.e[0];
+	e[1] *= v.e[1];
+	e[2] *= v.e[2];
+	return *this;
+}
+
+inline Vec3& Vec3::operator/=(const Vec3 &v)
+{
+	e[0] /= v.e[0];
+	e[1] /= v.e[1];
+	e[2] /= v.e[2];
+	return *this;
+}
+
+inline Vec3& Vec3::operator*=(const float t)
+{
+	e[0] *= t;
+	e[1] *= t;
+	e[2] *= t;
+	return *this;
+}
+
+inline Vec3 unit_vector(Vec3 v)
+{
+	return v / v.length();
+}
+
 #endif /* !Vec3_HPP_ */
