0003754ffd2a4cf4a35704db3ecae3d4d71a16a0
Emna <emna.boukadida@epitech.eu>
2024-05-03 14:12:02 +0200

    [~] fix the interface

--- src/Decorator/IDecorator.hpp
+++ src/Decorator/IDecorator.hpp
@@ -9,14 +9,14 @@
 #define IDECORATOR_HPP_
 
 #include "IMaterial.hpp"
+#include "IHitable.hpp"
 
 class IDecorator {
     public:
         virtual ~IDecorator() = default;
         //virtual void setTexture() = 0; //not sure
     private:
-    std::unique_ptr<IMaterial> mat_ptr;
-
-};
-
+    std::unique_ptr<Objects> obj;
+    std::vector<std::unique_ptr<IHitable>> hitable;
+}
 #endif /* !IDECORATOR_HPP_ */
