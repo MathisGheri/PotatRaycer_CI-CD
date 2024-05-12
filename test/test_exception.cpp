6b732c252fc55721f486b7a4962ae58b1c6336e6
Emna <emna.boukadida@epitech.eu>
2024-05-12 18:07:19 +0200

    [+] added more tests and i have scene and scenebuilder tests but it doesn't want to work

--- test/test_exception.cpp
+++ test/test_exception.cpp
@@ -14,8 +14,22 @@ Test(Exception, GetMessage) {
     Exception ex("Test Exception");
     cr_assert(strcmp(ex.what(), "Test Exception") == 0);
 }
+Test(Exception, GetLevelDefault) {
+    Exception ex("Test Exception");
+    cr_assert(ex.getLevel() == Level::LOW);
+}
 
 Test(Exception, GetLevel) {
     Exception ex("Test Exception", Level::MIDDLE);
     cr_assert(ex.getLevel() == Level::MIDDLE);
+}
+
+Test(Exception, GetLevelHigh) {
+    Exception ex("Test Exception", Level::HIGH);
+    cr_assert(ex.getLevel() == Level::HIGH);
+}
+
+Test(Exception, GetLevelNone) {
+    Exception ex("Test Exception", Level::NONE);
+    cr_assert(ex.getLevel() == Level::NONE);
 }
